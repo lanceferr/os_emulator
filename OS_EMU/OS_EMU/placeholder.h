6ec1e34c12c056daacf47eb2b58f94fffa9ee957
Lanz Wynel Choi <160328136+Lanz20012@users.noreply.github.com>
2026-05-24 11:18:37 +0800

    minor formatting change

--- OS_EMU/OS_EMU/placeholder.h
+++ OS_EMU/OS_EMU/placeholder.h
@@ -3,22 +3,21 @@
 #include <string>
 
 void initializeCommand(std::string command) {
-	std::cout << "\ninitialize command recognized, doing something. \n" << std::endl;
+    std::cout << "\ninitialize command recognized. Doing something.\n" << std::endl;
 }
 
 void screenCommand(std::string command) {
-	std::cout << "\nscreen command recognized, doing something.\n" << std::endl;
+    std::cout << "\nscreen command recognized. Doing something.\n" << std::endl;
 }
 
 void schedulerStartCommand(std::string command) {
-	std::cout << "\nscheduler start command recognized, doing something.\n" << std::endl;
+    std::cout << "\nscheduler-start command recognized. Doing something.\n" << std::endl;
 }
 
 void schedulerStopCommand(std::string command) {
-	std::cout << "\nscheduler stop command recognized, doing something.\n" << std::endl;
+    std::cout << "\nscheduler-stop command recognized. Doing something.\n" << std::endl;
 }
 
 void reportUtilCommand(std::string command) {
-	std::cout << "\nreport util command recognized, doing something.\n" << std::endl;
+    std::cout << "\nreport-util command recognized. Doing something.\n" << std::endl;
 }
-
